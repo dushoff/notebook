506083717abf9a2b66cdeac991e658763561ec53
Jonathan Dushoff <dushoff@mcmaster.ca>
2021-02-18 18:20:20 -0500

    good night

--- sandbox.R
+++ sandbox.R
@@ -1,6 +1,10 @@
 
-x <- exp(seq(-3, 3, by=0.4))
 x <- seq(2, 10, by=0.04)
+x <- exp(seq(-3, 3, by=0.4))
 y = x/log10(x)
+
+## data.frame(x[-length(x)], diff(x))
+log(1+diff(x)/x[-length(x)])
+diff(log(x))
 
 plot(x, y, type="l")
