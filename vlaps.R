669e094fe05e0c503700802f41c14b4027f54f0a
Jonathan Dushoff <dushoff@mcmaster.ca>
2021-09-29 22:48:58 -0400

    restarted

--- vlaps.R
+++ vlaps.R
@@ -7,13 +7,16 @@ vlap <- (csvRead("drivers")
 	%>% left_join(csvRead("times"))
 	%>% left_join(csvRead("races"), by="raceId")
 	%>% left_join(csvRead("circuits"), by="circuitId")
+	%>% left_join(csvRead("pit"), by=c("raceId", "driverId", "lap"))
 	%>% filter(year >= 2018)
 	%>% transmute(NULL
 		, race=name.x, circuit=name.y
-		, year, lap, position, time=milliseconds/1000
+		, year, lap, position, time=milliseconds.x/1000
+		, pit_time=milliseconds.y/1000
 	)
 	%>% mutate(across(where(is.character), ~fct_rev(fct_infreq(.))))
 	%>% filter(time<300)
 )
+summary(vlap)
 csvSave(vlap)
 rdsSave(vlap)
