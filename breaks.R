dcdea3f3c5fb75db1f9e9db8b3837806f2a398e2
Jonathan Dushoff <dushoff@mcmaster.ca>
2022-03-29 18:55:29 -0400

    limBreaks to control the excessive tendencies of axisTicks

--- breaks.R
+++ breaks.R
@@ -10,13 +10,22 @@ log_breaks(n=4)(c(1, 1.2))
 axisTicks(nint=4, log=TRUE, usr=log(c(1, 20)))
 
 ## The best I can do for now.
+
+limBreaks <- function(v, n=5){
+	b <- axisTicks(nint=n, log=TRUE, usr=range(v))
+	upr <- min(b[log(b)>=max(v)])
+	lwr <- max(b[log(b)<=min(v)])
+	print(c(lwr=lwr, upr=upr))
+	return(b[(b>=lwr) & (b<=upr)])
+}
+
 divmultbreaks <- function(v, n=6, nmin=3, anchor=TRUE){
 	if (anchor) v <- unique(c(v, 1))
 	v <- log(v)
 	neg <- min(v)
-	if (neg==0) return(axisTicks(nint=n, log=TRUE, usr=range(v)))
+	if (neg==0) return(limBreaks(v, n))
 	pos <- max(v)
-	if (pos==0) return(1/axisTicks(nint=n, log=TRUE, usr=-range(v)))
+	if (pos==0) return(1/limBreaks(-v, n))
 
 	flip <- -neg
 	big <- pmax(pos, flip)
@@ -24,10 +33,10 @@ divmultbreaks <- function(v, n=6, nmin=3, anchor=TRUE){
 	bigprop <- big/(pos + flip)
 	bigticks <- ceiling(n*bigprop)
 
-	main <- axisTicks(nint=bigticks, log=TRUE, usr=c(0, big))
+	main <- limBreaks(c(0, big), bigticks)
 	cut <- pmin(bigticks, 1+sum(main<small))
 	if(cut<nmin)
-		other <- axisTicks(nint=nmin, log=TRUE, usr=c(0, small))
+		other <- limBreaks(c(0, small), nmin)
 	else
 		other <- main[1:cut]
 
@@ -42,4 +51,8 @@ divmultbreaks(c(0.04))
 divmultbreaks(c(0.04, 11))
 divmultbreaks(c(0.02, 2))
 divmultbreaks(c(0.8, 20))
+
+y <- (exp(seq(-2,5,0.2)))
+print(y)
+divmultbreaks(y)
 
