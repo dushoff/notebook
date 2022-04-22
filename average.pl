ff8cb1ca97c7917fae1c5d6ff02237503d1bb752
dushoff <dushoff@mcmaster.ca>
2022-04-22 17:57:17 -0400

    homing (fiVe)

--- average.pl
+++ average.pl
@@ -1,24 +1,21 @@
-use strict;
-use 5.10.0;
-
 my $base=100;
-my @first;
+my @N;
 
 sub tree{
-	my $num = shift @_;
-	my @low = @{shift @_};
-	my @high = @{shift @_};
-	my @mid = ($low[0]+$high[0], $low[1]+$high[1]);
-	$mid[2] = int($base*$mid[0]/$mid[1] + 1/2);
-	$first[$mid[2]] = $mid[1] unless defined $first[$mid[2]];
-	# return if $num >=$nmax;
-	tree($num+1, \@low, \@mid) unless $mid[2]-$low[2]<=1;
-	tree($num+1, \@mid, \@high) unless $high[2]-$mid[2]<=1;
+   my $num = shift @_;
+   my @low = @{shift @_};
+   my @high = @{shift @_};
+   my @mid = ($low[0]+$high[0], $low[1]+$high[1]);
+   $mid[2] = int($base*$mid[0]/$mid[1] + 1/2);
+   $N[$mid[2]] = $mid[1] unless defined $N[$mid[2]];
+   # return if $num >=$nmax;
+   tree($num+1, \@low, \@mid) unless $mid[2]-$low[2]<=1;
+   tree($num+1, \@mid, \@high) unless $high[2]-$mid[2]<=1;
 }
 
-$first[0] = $first[$base] = 1;
+$N[0] = $N[$base] = 1;
 tree(0, [0, 1, 0], [1, 1, $base]); 
 
-for my $i (0..$#first){
-	say "$i $first[$i]";
+for my $n (0..$#N){
+   print "$n $N[$n]\n";
 }
